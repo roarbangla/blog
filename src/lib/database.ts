3d290b8031a30e440645d09b383c8948cb01fce8
Saiful Alam Rakib <msa4rakib@gmail.com>
2025-12-07 00:33:42 +0600

    Update Prisma configuration and dependencies for PostgreSQL adapter

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,6 +1,7 @@
-// lib/database.ts
-import { PrismaClient } from "@src/generated/prisma";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/client'
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+export const prisma = new PrismaClient({ adapter })
 
 export default prisma;
