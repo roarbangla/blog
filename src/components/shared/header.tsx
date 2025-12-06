1b891a04dd95fde974484faf08ca14c0dde49d17
Saiful Alam Rakib <msa4rakib@gmail.com>
2025-12-06 23:08:02 +0600

    chore: add .yarnrc configuration and add beta tag

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -2,16 +2,18 @@ import { Menu, Search } from "lucide-react";
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import { Badge } from "../ui/badge";
 
 export const Header = () => {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex h-16 items-center justify-between px-4 md:px-6">
         {/* Logo */}
-        <Link href="/" className="flex items-center space-x-2">
+        <Link href="/" className="flex relative items-center space-x-2">
           <span className="text-2xl font-bold tracking-tight text-primary font-playfair">
             roar<span className="text-orange-500 ml-1 text-normal">বাংলা</span>
           </span>
+          <Badge variant="outline" className="absolute top-0 -right-10 text-xs text-orange-500">Beta</Badge>
         </Link>
 
         {/* Search Bar - Desktop */}
